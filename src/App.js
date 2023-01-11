3a4b28686b87b240016efc5e7d8b43606895089d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-01-11 04:36:49 -0800

    postcations redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import About from "./component/About";
 import "./App.css";
+import SummaryCard from "./component/SummaryCard";
 
 function App() {
   return (
     <div className="App">
       <About />
+      <SummaryCard/>
     </div>
   );
 }
